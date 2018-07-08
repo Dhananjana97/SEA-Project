580920f18f059858acdc893ca3a48bf4044a5aeb
pinsarakweerasinghe <pinsarakweerasinghe@gmail.com>
2018-07-08 10:12:43 +0530

    registration corrections

--- RegisterForExamStudent.php
+++ RegisterForExamStudent.php
@@ -4,19 +4,22 @@ if(!isset($_SESSION['user'])|| $_SESSION['user']->type!="student"){header('Locat
         <h1>Register for exams</h1>
         <p>Fill the following form to register. Do not make mistakes. look up again before click register button.</p>       
           <div class="form_settings" >
-		  <form action="Student\DashboardPage\RegisterForExam\reg.php" method="get">
+		  <form action="Student\DashboardPage\RegisterForExam\reg.php" method="get" onsubmit="return Validate()" name="vform">
 
 			</br></br>
-			<p><span>Index No.</span><input  type="text" name="indexno" value="" /></p>
-			<p><span>Full Name</span><input  type="text" name="fname" value="" /></p>
+              <div><p><span>Index No.</span><input  type="text" name="indexno" value="" placeholder="Enter Index Number"/></p>
+                  <div id="indexno_error" class="val_error"></div></div>
+              <div><p><span>Name</span><input  type="text" name="fname" value="" placeholder="Enter Your Full Name"/></p>
+                  <div id="fname_error" class="val_error"></div></div>
 			
-			<p><span>Department</span></p>
+			<div><p><span>Department</span></p>
 			<select name="dept">
 				<option value="cse">Computer Science and Engineering</option>
 				<option value="ee">Electrical Engineering</option>
 				<option value="ce">Civil Engineering</option>
-			</select></br></br>
-			
+            </select>
+                <div id="dept_error" class="val_error"></div></div></br></br>
+			<div>
 			<span><input type="radio" name="sem" value="1"></span>
 			<label>1st Semester</label><br>
 			<span><input type="radio" name="sem" value="2"></span>
@@ -26,8 +29,10 @@ if(!isset($_SESSION['user'])|| $_SESSION['user']->type!="student"){header('Locat
 			<span><input type="radio" name="sem" value="4"></span>
 			<label>4th Semester</label></br></br>
 
-			<p><span>Other notes	</span><textarea class="contact textarea" rows="8" cols="50" name="onotes"></textarea></p>
-			<p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="contact_submitted" value="Register" /></p>
+                <div id="sem_error" class="val_error"></div></div>
+              <div><p><span>Other notes	</span><textarea class="contact textarea" rows="8" cols="50" name="onotes" placeholder="Enter other notes"></textarea></p>
+                  <div id="onotes_error" class="val_error"></div></div>
+              <div><p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" name="contact_submitted" value="Register" /></p></div>
 			</form>
             
             
@@ -43,3 +48,80 @@ if(!isset($_SESSION['user'])|| $_SESSION['user']->type!="student"){header('Locat
   </div>
 </body>
 </html>
+
+<script type="text/javascript">
+    var indexno=document.forms["vform"]["indexno"];
+    var fname=document.forms["vform"]["fname"];
+    var dept=document.forms["vform"]["dept"];
+    var sem=document.forms["vform"]["sem"];
+   // var onotes=document.forms["vform"]["onotes"];
+
+    var indexno_error=document.getElementById("indexno_error");
+    var fname_error=document.getElementById("fname_error");
+    var dept_error=document.getElementById("dept_error");
+    var sem_error=document.getElementById("sem_error");
+   // var onotes_error=document.getElementById("onotes_error");
+
+    indexno.addEventListener("blur",indexnoVerify,true);
+    fname.addEventListener("blur",fnameVerify,true);
+    dept.addEventListener("blur",deptVerify,true);
+    sem.addEventListener("blur",semVerify,true);
+   // onotes.addEventListener("blur",onotesVerify,true);
+
+    function Validate() {
+        if(indexno.value==""){
+            indexno.style.border="1px solid red";
+            indexno_error.textContent="Index No: required";
+            indexno.focus();
+            return false;
+        }
+        if(fname.value==""){
+            fname.style.border="1px solid red";
+            fname_error.textContent="Full Name required";
+            indexno.focus();
+            return false;
+        }
+        if(dept.value==""){
+            dept.style.border="1px solid red";
+            dept_error.textContent="department required";
+            dept.focus();
+            return false;
+        }
+        if(sem.value==""){
+            sem.style.border="1px solid red";
+            sem_error.textContent="semester required";
+            sem.focus();
+            return false;
+        }
+    }
+function indexnoVerify() {
+    if(indexno.value!=""){
+        indexno.style.border="1px solid #5E6E66";
+        indexno_error.innerHTML="";
+        return true;
+    }
+}
+    function fnameVerify() {
+        if(fname.value!=""){
+            fname.style.border="1px solid #5E6E66";
+            fname_error.innerHTML="";
+            return true;
+        }
+    }
+    function deptVerify() {
+        if(dept.value!=""){
+            dept.style.border="1px solid #5E6E66";
+            dept_error.innerHTML="";
+            return true;
+        }
+    }
+    function semVerify() {
+        if(sem.value!=""){
+            sem.style.border="1px solid #5E6E66";
+            sem_error.innerHTML="";
+            return true;
+        }
+    }
+
+
+</script>
