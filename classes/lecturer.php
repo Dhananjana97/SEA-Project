72ebb1404fdd2a5427b300de9b6a2395fa63ae0c
Tharindu Weerasinghe <tharinduwastaken@gmail.com>
2018-07-08 21:29:23 +0530

    Add files via upload

--- classes/lecturer.php
+++ classes/lecturer.php
@@ -2,7 +2,7 @@
 class Lecturer{
 	public $id;
 	public $type;
-	public $links = array('Assignments' => 'assignment.php', 'Recorrection' => 'recorrection.php', 'Set QUIZ' => 'quiz_home.php?content=QC', 'Result' => 'updateresults.php');
+	public $links = array('Assignments' => 'assignment.php', 'Recorrection' => 'lec_recorrection.php', 'Set QUIZ' => 'quiz_home.php?content=QC', 'Result' => 'updateresults.php');
 	function __construct($id){
 		$this->id = $id;
 		$this->type ='lecturer';
