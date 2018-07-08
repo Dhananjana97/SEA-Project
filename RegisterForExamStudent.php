4ed55b112112ada05228286d4145ddc179b9fc23
pinsarakweerasinghe <pinsarakweerasinghe@gmail.com>
2018-07-08 22:56:33 +0530

    ff

--- RegisterForExamStudent.php
+++ RegisterForExamStudent.php
@@ -73,31 +73,22 @@ if(!isset($_SESSION['user'])|| $_SESSION['user']->type!="student"){header('Locat
     var fname=document.forms["vform"]["fname"];
     var dept=document.forms["vform"]["dept"];
     var sem=document.forms["vform"]["sem"];
-<<<<<<< HEAD
-  var onotes=document.forms["vform"]["onotes"];
-=======
    // var onotes=document.forms["vform"]["onotes"];
->>>>>>> e6be4260ce1376ba232182d38fbc516fa9dbb4da
+
 
     var indexno_error=document.getElementById("indexno_error");
     var fname_error=document.getElementById("fname_error");
     var dept_error=document.getElementById("dept_error");
     var sem_error=document.getElementById("sem_error");
-<<<<<<< HEAD
-  var onotes_error=document.getElementById("onotes_error");
-=======
+
    // var onotes_error=document.getElementById("onotes_error");
->>>>>>> e6be4260ce1376ba232182d38fbc516fa9dbb4da
 
     indexno.addEventListener("blur",indexnoVerify,true);
     fname.addEventListener("blur",fnameVerify,true);
     dept.addEventListener("blur",deptVerify,true);
     sem.addEventListener("blur",semVerify,true);
-<<<<<<< HEAD
-  onotes.addEventListener("blur",onotesVerify,true);
-=======
    // onotes.addEventListener("blur",onotesVerify,true);
->>>>>>> e6be4260ce1376ba232182d38fbc516fa9dbb4da
+
 
     function Validate() {
         if(indexno.value==""){
@@ -112,36 +103,30 @@ if(!isset($_SESSION['user'])|| $_SESSION['user']->type!="student"){header('Locat
             indexno.focus();
             return false;
         }
-<<<<<<< HEAD
+
         if(dept.value==0){
-=======
-        if(dept.value==""){
->>>>>>> e6be4260ce1376ba232182d38fbc516fa9dbb4da
             dept.style.border="1px solid red";
             dept_error.textContent="department required";
             dept.focus();
             return false;
         }
-<<<<<<< HEAD
+
         if(sem.value=="0"){
-=======
-        if(sem.value==""){
->>>>>>> e6be4260ce1376ba232182d38fbc516fa9dbb4da
+
             sem.style.border="1px solid red";
             sem_error.textContent="semester required";
             sem.focus();
             return false;
         }
-<<<<<<< HEAD
-        if(false){
+
+        if(isNaN(indexno.value.substr(0,6))){
             indexno.style.border="1px solid red";
             indexno_error.textContent="Index No: not valied!";
             indexno.focus();
             return false;
         }
 
-=======
->>>>>>> e6be4260ce1376ba232182d38fbc516fa9dbb4da
+
     }
 function indexnoVerify() {
     if(indexno.value!=""){
